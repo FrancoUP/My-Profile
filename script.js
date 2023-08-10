99965f59814044c9f43f4d04df6c73220a0ff180
Francesco <114874755+FrancoUP@users.noreply.github.com>
2023-08-10 20:52:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -39,10 +39,35 @@ const boxProject = document.querySelectorAll(".box-project");
 
 const nameProject = document.querySelectorAll(".name");
 
-const viewCodeBtn = document.querySelectorAll(".viewCodeBtn")
+const viewCodeBtn = document.querySelectorAll(".viewCodeBtn");
+
+const time = document.querySelectorAll(".time");
 
 
 //////////////////////////////////////////////////////////////////////////////////
+
+// get amount time from the start
+(function getTime() {
+
+  const startDate = new Date("October 01, 2022");
+  const newDate = new Date();
+  const tempo = newDate.getTime() - startDate.getTime();
+  const months  = Math.floor(tempo / (30 * 24 * 60 * 60 * 1000));
+
+  [...time].forEach( el => {
+
+    if(months < 12){
+      el.innerHTML = `${months} months experience`;
+    } else if(months >=  12 && months <  24) {
+      el.innerHTML = `1 year experience`;
+    } else if(months >= 24) {
+      el.innerHTML = `${months / 12} years experience`;
+    }
+  })
+
+})();
+
+
 
 // Scrool the page to the top
 window.addEventListener('load', function() {
